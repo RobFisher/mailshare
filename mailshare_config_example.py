3e545a30752f61bba404097b4081bf9a7e202728
Rob Fisher <rfisher@nds.com>
2011-09-22 17:52:26 +0100

    Download 10 emails from an IMAP server.

---
+++ mailshare_config_example.py
@@ -0,0 +1,8 @@
+# copy this file to mailshare_config.py, then edit the configuration parameters
+
+# IMAP settings
+imap_server = 'my_imap_server.com'
+imap_username = 'user@my_imap_server.com'
+imap_password = b'my_imap_password'
+imap_mailbox = 'INBOX'
+
---
+++ poll_imap_email.py
@@ -0,0 +1,23 @@
+# License: https://github.com/RobFisher/mailshare/blob/master/LICENSE
+
+import imaplib
+import mailshare_config
+import pprint
+
+server = imaplib.IMAP4_SSL(mailshare_config.imap_server)
+server.login(mailshare_config.imap_username, mailshare_config.imap_password)
+server.select(mailshare_config.imap_mailbox, readonly = True)
+typ, message_ids = server.search(None, 'ALL')
+
+# message_ids is a list with one item, so it looks like this:
+# [b'1 2 3 4 5']
+# To get a maximum number of mails we need to split it out into a list of
+# ids.
+max_emails_for_test = 10
+message_ids_to_fetch = message_ids[0].split()[0:max_emails_for_test]
+
+for message_id in message_ids_to_fetch:
+    typ, message_data = server.fetch(message_id, '(RFC822)')
+    for part in message_data:
+        pprint.pprint(part)
+
