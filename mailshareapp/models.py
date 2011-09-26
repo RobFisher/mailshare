b13082570e966d3939b0822cbf6c0a18bf51458c
Rob Fisher <rfisher@nds.com>
2011-09-26 16:33:37 +0100

    Database model for emails and addressees.

--- mailshareapp/models.py
+++ mailshareapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Addressee(models.Model):
+    name = models.TextField()
+    address = models.TextField()
+
+class Mail(models.Model):
+    sender = models.ForeignKey(Addressee, related_name='sent_mails')
+    to = models.ManyToManyField(Addressee, related_name='received_mails')
+    cc = models.ManyToManyField(Addressee, related_name='cced_mails')
+    subject = models.TextField()
+    date = models.DateTimeField()
+    message_id = models.TextField()
+    thread_index = models.TextField()
+    body = models.TextField()
