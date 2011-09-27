0a4acf61ab00665edd5a7deeea1ba46324603258
Rob Fisher <rfisher@nds.com>
2011-09-27 18:09:32 +0100

    Improve the admin interface

--- mailshareapp/admin.py
+++ mailshareapp/admin.py
@@ -1,4 +1,18 @@
-from mailshareapp.models import Mail
+from mailshareapp.models import Mail, Addressee
 from django.contrib import admin
 
-admin.site.register(Mail)
+class MailAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Addressees', {'fields': ['sender', 'to', 'cc']}),
+        ('Details', {'fields': ['subject', 'date']}),
+        ('Indexing', {'fields': ['message_id', 'thread_index'], 'classes': ['collapse']}),
+        (None, {'fields': ['body']}),
+    ]
+    list_display = ('sender', 'date', 'subject')
+    list_display_links = ['date']
+    list_filter = ['date']
+    date_hierarchy = 'date'
+    search_fields = ['subject', 'body']
+
+admin.site.register(Mail, MailAdmin)
+admin.site.register(Addressee)
