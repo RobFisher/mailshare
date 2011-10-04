388c37aa2c86ad0fa00f5faf5ad3c8e9d03d1aaf
RobFisher <robfisher@gmail.com>
2011-10-04 23:49:40 +0100

    Add console logging for Dajaxice

--- settings_example.py
+++ settings_example.py
@@ -145,6 +145,7 @@ INSTALLED_APPS = (
 # the site admins on every HTTP 500 error.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+# - Mailshare adds console logging for Dajaxice
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -152,12 +153,21 @@ LOGGING = {
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler'
         }
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
+            'propagate': True,
+        },
+        'dajaxice': {
+            'handlers': ['console'],
+            'level': 'INFO',
             'propagate': True,
         },
     }
