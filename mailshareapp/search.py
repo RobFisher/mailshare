1910a8f37399d0905b3eff2e024fafcff1eda3c4
Rob Fisher <rfisher@nds.com>
2011-10-26 10:08:26 +0100

    Parameter class to simplify code

--- mailshareapp/search.py
+++ mailshareapp/search.py
@@ -54,15 +54,23 @@ def _get_mail_id_html(i):
     return ''
 
 
+class _Parameter:
+    """Operations that can be performed on a search parameter."""
+    def __init__(self, get_query_func, get_html_func):
+        self.get_query = get_query_func
+        self.get_html = get_html_func
+
+
 _parameters_map = {
-    'query': (_get_full_text_query, _get_full_text_html),
-    'tag_id': (_get_tag_id_query, _get_tag_id_html),
-    'sender': (_get_sender_id_query, _get_sender_id_html),
-    'mail_id': (_get_mail_id_query, _get_mail_id_html),
+    'query': _Parameter(_get_full_text_query, _get_full_text_html),
+    'tag_id': _Parameter(_get_tag_id_query, _get_tag_id_html),
+    'sender': _Parameter(_get_sender_id_query, _get_sender_id_html),
+    'mail_id': _Parameter(_get_mail_id_query, _get_mail_id_html),
 }
 
 
 class Search:
+    """Represents a search and can convert between various representations of a search."""
     def __init__(self, request):
         """Create a new search object based on the specified request object."""
         self.query = None;
@@ -74,8 +82,8 @@ class Search:
             field_name = request_items[0][0]
             field_value = request_items[0][1]
             if field_name in _parameters_map:
-                self.query = _parameters_map[field_name][0](field_value)
-                self.html = _parameters_map[field_name][1](field_value)
+                self.query = _parameters_map[field_name].get_query(field_value)
+                self.html = _parameters_map[field_name].get_html(field_value)
 
         if self.query != None:
             self.results = Mail.objects.filter(self.query)
